83f2554da0476421aba41c39228a89b8c5b59f89
sandhya04sandhya <140478145+sandhya04sandhya@users.noreply.github.com>
2023-08-25 13:16:22 +0530

    Add files via upload

---
+++ Reverseprogram.java
@@ -0,0 +1,22 @@
+package string;
+
+import java.util.Scanner;
+
+public class Reverseprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String s;  
+		Scanner sc=new Scanner(System.in);  
+		System.out.print("Enter a String: ");  
+		s=sc.next();                 //reading string from user  
+		System.out.print("After reverse string is: ");  
+		for(int i=s.length();i>0;--i)                //i is the length of the string  
+		{  
+		System.out.print(s.charAt(i-1));            //printing the character at index i-1  
+
+	}
+
+}}
